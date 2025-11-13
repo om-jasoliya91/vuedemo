b8b6668255f39ee4c45bd4045007034f39eb1974
developer <patoliya.oj@gmail.com>
2025-11-13 18:07:21 +0530

    nextTick( how works and how async and await works)

--- src/components/practise/ReactivityExample.vue
+++ src/components/practise/ReactivityExample.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, nextTick } from 'vue';
 
 const obj = ref({
   nested: { count: 0 },
@@ -9,11 +9,24 @@ function mutateDeeply() {
   obj.value.nested.count++,
     obj.value.arr.push('ketan', 'om')
 }
+
+// Count variable for increment()
+
+const count = ref(0)
+async function increment() {
+  count.value++ //it is update existing value
+  await nextTick() //it is wait DOM update
+  console.log('DOM updated! New count:', count.value) //It is finally display in consol check Now DOM is updated
+  //Now the DOM is Updated
+}
+
 </script>
 
 <template>
   <button @click="mutateDeeply">Mutate</button>
   <p>{{ obj }}</p>
+
+  Total Count:<button @click="increment">{{ count }}</button>
 </template>
 
 <style scoped></style>
