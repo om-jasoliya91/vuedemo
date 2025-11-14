c60079b02d18f165bd2e33d173dc0fa329aac45f
developer <patoliya.oj@gmail.com>
2025-11-14 08:55:24 +0530

    it is use reactiv and proxy and diect object how works

--- src/components/practise/ReactivityExample.vue
+++ src/components/practise/ReactivityExample.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, nextTick } from 'vue';
+import { ref, nextTick, reactive, toRefs } from 'vue';
 
 const obj = ref({
   nested: { count: 0 },
@@ -19,14 +19,36 @@ async function increment() {
   console.log('DOM updated! New count:', count.value) //It is finally display in consol check Now DOM is updated
   //Now the DOM is Updated
 }
+//it is object it is proxy means reactive it is use automatic update ui
+const state = reactive({
+  count: 0,
+  user: {
+    name: 'om'
+  }
+})
+const { count: countRef, user } = toRefs(state)
+
+function incrementReactive() {
+  // state.count++
+  console.log(state.count++)
+}
+
+const raw = {}
+const proxy = reactive(raw)
+// proxy is NOT equal to the original.
+console.log(proxy === raw) // false
+console.log(reactive(raw) === proxy) //true
 
 </script>
 
 <template>
   <button @click="mutateDeeply">Mutate</button>
   <p>{{ obj }}</p>
-
-  Total Count:<button @click="increment">{{ count }}</button>
+  Total Count: <button @click="increment">{{ count }}</button>
+  <br><br>
+  <p>Reactive Count From toRefs(): {{ countRef }}</p>
+  <p>User Name: {{ user.name }}</p>
+  <button @click="incrementReactive">Increase Reactive Count</button>
 </template>
 
 <style scoped></style>
