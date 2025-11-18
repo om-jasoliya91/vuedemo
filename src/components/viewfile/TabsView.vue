de7477f351817852b6397ea845477cf808506165
developer <patoliya.oj@gmail.com>
2025-11-18 10:10:22 +0530

    onUpdated example

--- src/components/viewfile/TabsView.vue
+++ src/components/viewfile/TabsView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, onUpdated } from 'vue'
 import LoginView from '../practise/LoginView.vue'
 import RegiView from '../practise/RegiView.vue'
 
@@ -13,12 +13,25 @@ onMounted(() => {
     console.log('Example of onMounted with component lifecycle')
   }, 3000)
 })
+
+const count = ref(0)
+const message = ref('hello')
+
+onUpdated(() => {
+  console.log('DOM updates because count changed!')
+  console.log('updated DOM text:', document.getElementById('msg').innerText)
+})
 </script>
 
 <template>
   <component :is="components[currentComponents]"> </component>
   <button @click="currentComponents = 'LoginView'">Login</button>
   <button @click="currentComponents = 'RegiView'">Register</button>
+
+  <button @click="count++">Count:{{ count }}</button>
+
+  <p id="msg">{{ message }}</p>
+  <button @click="message = 'World'">Change</button>
 </template>
 
 <style scoped></style>
