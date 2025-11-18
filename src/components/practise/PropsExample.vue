2a865d8ff921d10019dea50bd9acd9d76773b1aa
developer <patoliya.oj@gmail.com>
2025-11-18 15:52:10 +0530

    props validation

--- src/components/practise/PropsExample.vue
+++ src/components/practise/PropsExample.vue
@@ -5,7 +5,44 @@ const emit = defineEmits(['updatesFoo']) //child can send event
 const { foo, id, title } = defineProps({
   foo: String,
   id: Number,
+  //normal basic type check
   title: String,
+
+  //multiple Allowed types
+  propB: [String, Number],
+
+  //Required string
+  propC: { type: String, required: true },
+
+  //required but nullable string
+  propD: {
+    type: [String, null],
+    required: true,
+  },
+  //Number with default value
+  propE: {
+    type: Number,
+    default: 100,
+  },
+  // Object with default value
+  propF: {
+    type: Object,
+    default(rawProps) {
+      return { message: 'hello' }
+    },
+  },
+  // Custom validator
+  propG: {
+    validator(value, props) {
+      return ['success', 'warning', 'danger'].includes(value)
+    },
+  },
+  propH: {
+    type: Function,
+    default() {
+      return 'Default function'
+    },
+  },
 })
 function requestToChange() {
   emit('updatesFoo', 'child wants too change foo!')
@@ -26,6 +63,16 @@ watch(
 
   <br />
   <button @click="requestToChange">Request to foo update</button>
+
+  <div>
+    <p>B: {{ propB }}</p>
+    <p>C: {{ propC }}</p>
+    <p>D: {{ propD }}</p>
+    <p>E: {{ propE }}</p>
+    <p>F: {{ propF }}</p>
+    <p>G: {{ propG }}</p>
+    <p>H: {{ propH() }}</p>
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/viewfile/refView.vue
+++ src/components/viewfile/refView.vue
@@ -1,29 +1,36 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+
 import refExample from '../practise/refExample.vue'
 import ChildCard from '../practise/ChildCard.vue'
 import ClickButton from './ClickButton.vue'
 import BlogPost from '../practise/BlogPost.vue'
 import AlertBox from '../practise/AlertBox.vue'
 import PropsExample from '../practise/PropsExample.vue'
-import { ref, onMounted } from 'vue'
 
+// child method calling (ref example)
 const child = ref(null)
 
 onMounted(() => {
-  child.value.sayHello() //calling child method
+  if (child.value?.sayHello) {
+    child.value.sayHello()
+  }
 })
 
+// posts data
 const posts = ref([
   { id: 1, title: 'My Vue Journey' },
   { id: 2, title: 'Blogging with Vue' },
 ])
 
+// blog font change
 const postFontSize = ref(1)
 
 function enlargedText() {
   postFontSize.value += 0.1
 }
 
+// props example foo update
 const fooValue = ref('hello')
 
 function changeFoo(newValue) {
@@ -32,38 +39,70 @@ function changeFoo(newValue) {
 </script>
 
 <template>
-  <refExample></refExample>
-  <ChildCard ref="child"></ChildCard>
-  <ChildCard name="om" :score="60" />
+  <refExample />
+
+  <!-- Child with required props -->
+  <ChildCard ref="child" name="om" :score="60" />
+
+  <!-- Loop Child Cards -->
   <ChildCard v-for="post in posts" :key="post.id" :title="post.title" />
 
-  <ClickButton></ClickButton>
-  <ClickButton></ClickButton>
-  <ClickButton></ClickButton>
-  <br />
-  <br />
+  <!-- ClickButton examples -->
+  <ClickButton />
+  <ClickButton />
+  <ClickButton />
+
+  <br /><br />
+
+  <!-- BlogPost enlarge example -->
   <div :style="{ fontSize: postFontSize + 'em' }">
-    <!-- here i got parse error because  not write stringin single quote-->
-    <BlogPost :title="'My string'" @enlarged="enlargedText"></BlogPost>
+    <BlogPost title="My string" @enlarged="enlargedText" />
   </div>
 
-  <br />
-  <br />
+  <br /><br />
+
+  <!-- Alert Box -->
   <div class="container text-dark">
-    <AlertBox type="warning">it is information alert</AlertBox>
+    <AlertBox type="warning"> it is information alert </AlertBox>
   </div>
 
+  <!-- Global component -->
   <GlobalComponent />
 
-  <PropsExample v-bind:foo="fooValue"></PropsExample>
+  <!-- PropsExample with simple foo -->
+  <PropsExample :foo="fooValue" :propC="'Required String'" :propD="null" />
 
-  <PropsExample v-for="post in posts" :key="post.id" v-bind="post" />
+  <!--here propC and propD always need apply because we can set validation required:true-->
+  <!-- Loop (avoid v-bind="post" unless props match) -->
+  <PropsExample
+    v-for="post in posts"
+    :key="post.id"
+    :id="post.id"
+    :title="post.title"
+    :propC="'Required String'"
+    :propD="null"
+  />
+
+  <!-- PropsExample changing foo -->
   <PropsExample
     :foo="fooValue"
     :id="1"
-    :title="'how are you'"
+    title="how are you"
     @updatesFoo="changeFoo"
-  ></PropsExample>
+    :propC="'Required String'"
+    :propD="null"
+  />
+
+  <!-- Full prop demo -->
+  <PropsExample
+    propB="Hello"
+    propC="RequiredString"
+    :propD="null"
+    :propE="50"
+    :propF="{ message: 'custom' }"
+    propG="warning"
+    :propH="() => 'Custom function'"
+  />
 </template>
 
 <style scoped></style>
